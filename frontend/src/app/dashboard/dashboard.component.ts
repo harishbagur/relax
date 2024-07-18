d9bcb966409f8bdfb10e6194250cf3d47074e717
Nahidakhtar1 <akhtar.nahid78697@gmail.com>
2024-07-19 01:20:08 +0530

    added

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -4,12 +4,28 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
 import {MatCardModule} from '@angular/material/card'
 import { FormsModule } from '@angular/forms';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent,MatCardModule,FormsModule],
+  imports: [CommonModule, RouterModule, HeaderComponent,MatCardModule,FormsModule,NgbCarouselModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  text: string = '';
+  recognizing: boolean = false;
+  recognition: any;
+
+  photos: Array<{src: string, title: string}> = [
+    { src: '/images/p1.jpg', title: 'Family Photo 1' },
+    { src: '/images/p2.jpg', title: 'Family Photo 2' },
+    { src: '/images/caretaker.jfif', title: 'Family Photo 2' },
+    { src: '/images/p1.jfif', title: 'Family Photo 2' },
+
+    // Add more photos here
+  ];
+
+}
