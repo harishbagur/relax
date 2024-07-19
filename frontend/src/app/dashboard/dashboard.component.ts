efea286fe59fa3f281ff9b37431b800e4f5c8825
Shivansh Singh <shivanshsingh1910@gmail.com>
2024-07-19 11:48:48 +0530

    added tts

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from "../header/header.component";
-import {MatCardModule} from '@angular/material/card'
+import { HeaderComponent } from '../header/header.component';
+import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
@@ -10,7 +10,14 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent,MatCardModule,FormsModule,NgbCarouselModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HeaderComponent,
+    MatCardModule,
+    FormsModule,
+    NgbCarouselModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
@@ -19,7 +26,7 @@ export class DashboardComponent {
   recognizing: boolean = false;
   recognition: any;
 
-  photos: Array<{src: string, title: string}> = [
+  photos: Array<{ src: string; title: string }> = [
     { src: '/images/p1.jpg', title: 'Family Photo 1' },
     { src: '/images/p2.jpg', title: 'Family Photo 2' },
     { src: '/images/caretaker.jfif', title: 'Family Photo 2' },
@@ -28,4 +35,12 @@ export class DashboardComponent {
     // Add more photos here
   ];
 
+  speak(text: string) {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = text;
+    speech.volume = 1; // 0 to 1
+    speech.rate = 1; // 0.1 to 10
+    speech.pitch = 1; //0 to 2
+    window.speechSynthesis.speak(speech);
+  }
 }
