9e38a4ff879e4085d5115b60129f1c546effa07a
Nahidakhtar1 <akhtar.nahid78697@gmail.com>
2024-07-19 13:40:38 +0530

    hh

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
